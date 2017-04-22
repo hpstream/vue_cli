e5a7fc492f7fd1c23267d51c6e6448651e06f8a0
hpstream <915414300@qq.com>
2017-04-22 08:34:59 +0800

    //部分案例部分

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-header></v-header>
+</template>
+
+<script>
+import head from './components/header/head';
+
+export default {
+  components: {
+    'v-header': head
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/header/head.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="header">
+        <div class="title">
+            {{title}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        'title': 'namesss1'
+    };
+  }
+};
+</script>
+
+<style  lang='less'>
+    .header{
+        background:red;
+        .title{
+            color:white;
+        }
+    }
+
+</style>
